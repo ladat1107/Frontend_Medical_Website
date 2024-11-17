602ab38c584cb77a66ed8705dd2ddcd9512a5d7b
canhnlse172651 <canhnlse172651@gmail.com>
2024-11-17 13:32:22 +0700

    build base UI user

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -19,11 +19,6 @@ import VideoComponent from "@/components/Video";
 const cx = classNames.bind(styles);
 
 const HomePage = () => {
-  const a = [
-    "http://benhvienkhuvucthuduc.vn/content/uploads/Banner/c954630d-00be-4760-89cb-6088dd1361a5/b78f4cd4-d939-4479-b4d8-b1e100e51f35_tieusoihuyet-noitk-goicapcuu123.jpg",
-    "http://benhvienkhuvucthuduc.vn/content/uploads/Banner/fc6e0240-20e3-4a28-a43d-b75ff511a016/16423e45-045c-41f8-b729-b1e100e5a753_banner-dangkykhamquaweb-web-update1900066883-02112023.jpg",
-    "http://benhvienkhuvucthuduc.vn/content/uploads/Banner/c954630d-00be-4760-89cb-6088dd1361a5/b78f4cd4-d939-4479-b4d8-b1e100e51f35_tieusoihuyet-noitk-goicapcuu123.jpg",
-  ];
   return (
     <>
       <Marquee />
